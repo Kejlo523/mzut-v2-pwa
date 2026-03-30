6e12b86b90a3b6f9f41fb49876cca865b95c7898
Kejlo523 <kejlo234@gmail.com>
2026-03-30 14:48:20 +0200

    Zaktualizuj ekran finansów

--- src/app/screens/StudyScreens.tsx
+++ src/app/screens/StudyScreens.tsx
@@ -646,7 +646,7 @@ export function FinanceScreen({
   })();
 
   return (
-    <section className="screen finance-screen">
+    <section className={`screen finance-screen${noticeOpen ? ' notice-open' : ''}`}>
       {showSkeleton ? (
         <FinanceLoadingSkeleton />
       ) : (
