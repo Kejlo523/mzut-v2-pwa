c79af99fff6b6c5f4cd7aa40a5e084d618567b23
Kejlo523 <kejlo234@gmail.com>
2026-04-03 21:20:03 +0200

    feat: add Manrope and Space Grotesk fonts to project

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
 import { createRoot } from 'react-dom/client';
 import { registerSW } from 'virtual:pwa-register';
+import '@fontsource/manrope/400.css';
+import '@fontsource/manrope/500.css';
+import '@fontsource/manrope/600.css';
+import '@fontsource/manrope/700.css';
+import '@fontsource/manrope/800.css';
+import '@fontsource/space-grotesk/500.css';
+import '@fontsource/space-grotesk/600.css';
+import '@fontsource/space-grotesk/700.css';
 import './index.css';
 import App from './App';
 
